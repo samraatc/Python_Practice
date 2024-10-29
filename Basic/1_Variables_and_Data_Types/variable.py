29e9566a4929d39ad87434c4580106f644de21a4
Bishnu001 <97343961+samraatc@users.noreply.github.com>
2024-10-29 08:56:56 +0000

    first commit

---
+++ Basic/1_Variables_and_Data_Types/variable.py
@@ -0,0 +1,24 @@
+# what is variable :- A variable is a name that refers to a value stored in memory, we can use any name to assign the variable. variable declearation have some specific rules to be flow 
+
+#  Data Types in Python
+# Numeric Types:- int, float, complex...
+# String 
+# Boolean 
+# Sequence Types  :- list, tuple, range...
+# Mapping Type :- dictionary...
+# Set Types :- set, frozenset...
+#    
+
+# Practice 
+# Create a variable named Use the type() function to check the types of the variables you have created. Create a string variable named greeting and assign it the value "Hello, World!".
+# A.Print the length of the string.
+# B.Convert the string to uppercase and print it
+
+
+age = 25        # An integer
+name = "Alice"  # A string
+height = 5.6    # A float
+is_student = True  # A boolean
+
+
+
